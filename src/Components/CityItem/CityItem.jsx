f44284edd8770bff3c2414f53c6ed88d0e7c8297
Dawid <dawid.polednik@gmail.com>
2019-11-20 23:30:39 +0100

    added buttons

--- src/Components/CityItem/CityItem.jsx
+++ src/Components/CityItem/CityItem.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import styles from "./CityItem.module.scss";
 
-const CityItem = ({ name }) => <li>{name}</li>;
+const CityItem = ({ name }) => (
+  <li>
+    <div className={styles.CityItemContainer}>
+      <p>{name}</p>
+      <button>Display city information</button>
+    </div>
+  </li>
+);
 
 export default CityItem;
