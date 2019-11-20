a05249d3fa3ca8289f8853018df3dce2b38d05bf
Dawid <dawid.polednik@gmail.com>
2019-11-20 12:01:22 +0100

    extract only 10 cities by his pollution

--- src/actions/countryActions.js
+++ src/actions/countryActions.js
@@ -1,18 +1,28 @@
 export const SET_COUNTRY = "SET_COUNTRY";
 
-export const sortCities = (...cities) =>
-  cities.sort((a, b) => b.value - a.value);
+const _ = require("lodash");
+
+const removeDuplicatesObjects = (...cities) => _.uniqBy(cities[0], "city");
+
+const extractFirstTenCitiesNames = (...cities) =>
+  _.take(cities[0], 10).map(({ city }) => city);
 
 export const fetchCitiesNames = countryName => {
   return dispatch => {
     return fetch(
-      `https://api.openaq.org/v1/measurements?country=${countryName}&limit=10&parameter[]=pm25&sort=desc&order_by=value`,
+      `https://api.openaq.org/v1/measurements?country=${countryName}&limit=500&parameter[]=pm25&sort=desc&order_by=value`,
       {
         method: "GET"
       }
     )
       .then(r => r.json())
-      .then(({ results }) => results.map(({ city }) => console.log(city)));
+      .then(({ results }) => {
+        const withoutDuplicatesCities = removeDuplicatesObjects(results);
+        const extractedList = extractFirstTenCitiesNames(
+          withoutDuplicatesCities
+        );
+        return extractedList;
+      });
   };
 };
 
