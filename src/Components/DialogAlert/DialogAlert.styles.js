f8089af36180399349704b8e414c2b80c58524a9
Dawid <dawid.polednik@gmail.com>
2019-11-23 16:28:05 +0100

    ..

---
+++ src/Components/DialogAlert/DialogAlert.styles.js
@@ -0,0 +1,14 @@
+const style = () => ({
+  dialogTitle: {
+    textAlign: "center",
+    fontFamily: "roboto",
+    fontSize: "50px",
+    letterSpacing: "1px",
+    textTransform: "capitalize"
+  },
+  dialogContent: {
+    padding: "0px 40px 0px 40px",
+    textAlign: "left"
+  }
+});
+export default style;
