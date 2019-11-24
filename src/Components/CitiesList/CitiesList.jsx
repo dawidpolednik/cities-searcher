0f9a4e6c6157a3b9eeae0b2d6fc4d6d5ce759eb3
Dawid <dawid.polednik@gmail.com>
2019-11-24 14:37:46 +0100

    added animation on <ul>

--- src/Components/CitiesList/CitiesList.jsx
+++ src/Components/CitiesList/CitiesList.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import styles from "./CitiesList.module.scss";
 import CityItem from "../CityItem/CityItem";
+import ScrollAnimation from "react-animate-on-scroll";
 
 class CitiesList extends Component {
   render() {
@@ -10,12 +11,28 @@ class CitiesList extends Component {
         <h2
           className={styles.listTitle}
         >{`List of the 10 most polluted cities in ${country}`}</h2>
+        <ScrollAnimation
+          animateIn="fadeInRightBig"
+          initiallyVisible={false}
+          duration={1.5}
+          delay={0}
+          animateOnce
+          animatePreScroll
+          style={{
+            width: "100%",
 
-        <ul className={styles.citiesList}>
-          {citiesList.map((city, index) => (
-            <CityItem name={city} key={index} />
-          ))}
-        </ul>
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: 0
+          }}
+        >
+          <ul className={styles.citiesList}>
+            {citiesList.map((city, index) => (
+              <CityItem name={city} key={index} />
+            ))}
+          </ul>
+        </ScrollAnimation>
       </>
     );
   }
